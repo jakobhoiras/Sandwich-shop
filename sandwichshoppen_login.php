675a248e3a4413054149e7cc24865874b01de7a5
JanDickow <fjp124@alumni.ku.dk>
2014-05-10 13:31:53 +0200

    Database Oprettelse

--- sandwichshoppen_login.php
+++ sandwichshoppen_login.php
@@ -1,40 +1,82 @@
+<?php
+$db_username = "fjp124";
+$db_password = "FaxeKondi1";
+$db = "oci:dbname=//localhost:1521/dbwc";
+$user = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+$password2 = $_REQUEST['password2'];
+
+try {
+    $conn = new PDO($db, $db_username, $db_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo 'Caught exception: ', $e->getMessage(), "\n";
+}
+?>
 <html>
     <head>
         <title>Sandwich Shoppen</title>
     </head>
+    <?php
+    if ($password == $password2) {
+        if ($user != "" and $password != "") {
+            try {
+                $conn->beginTransaction();
+                $stmt = $conn->query("insert into as_users values('$user', '$password')");
+                $conn->commit();
+                echo "Du er nu oprettet som bruger :)";
+            } catch (Exception $ex) {
+                echo $ex->getMessage();
+                $conn->rollback();
+            }
+        }
+    } else {
+        echo "Passwords do not match, idiot";
+    }
+    ?>
     <body>
         <div id="container">
 
-            <div id="header" style="background-color:#04B431; height: 100px; line-height:2; width:73%; float:left; text-align: center;">
-                <font style="color:#FFFFFF; font-size: 50; text-align: center;">
+            <div id="header" style="background-color:#04B431; height: 100px; line-height:2; width:60%; float:left; text-align: right;">
+                <font style="color:#FFFFFF; font-size: 50;">
                 Sandwich Shoppen
                 </font>
             </div>
-            <div style="background-color:#04B431; height: 100px; line-height:2; width:27%; float:right;">
-                <center>Brugernavn:<input><br>
-                    Adgangskode:<input type="password"><br>
-                    <button>Log ind</button>
-                    <button>
-                        <a href="sandwichshoppen_login.php" style="text-decoration: none; color: #000000;">
-                            Ny bruger?</a>
-                    </button></center>
-            </div><hr>
-            <div id="menu" style="background-color:#04B431; width:100%; float:left; text-align:center">
-                <h3 style="color: black;">
-                    <a href="sandwichshoppen.php" style="text-decoration: none; color: #FFFFFF;">Forside </a>
-                    <a href="sandwichshoppen_menu.php" style="text-decoration: none; color: #FFFFFF;">Menu </a>
-                    <a href="sandwichshoppen_kontakt.php"  style="text-decoration: none; color: #FFFFFF;">Kontakt Information</a></h3>
-            </div>
-            <div align="center">
-                <img style="height: 400"src="http://abovethelaw.com/wp-content/uploads/2013/11/uncle-sam-we-want-you.jpg">
-                <h1>Oprettelse af ny bruger</h1>
+            <div style="background-color:#04B431; height: 100px; line-height:2; width:40%; float:right; text-align: right;">
                 Brugernavn:<input><br>
                 Adgangskode:<input type="password"><br>
-                Bekr&#230ft adgangskode:<input type="password"><br>
-                <button>Opret bruger</button><button><a href=sandwichshoppen.php style="text-decoration: none; color: #000000;">Allerede oprettet?</a></button>
+                <button style="width: 100px; background-color: #029727; border-top-left-radius: 10px; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; border-top-right-radius: 10px;">Log ind</button>
+                <a href="sandwichshoppen_login.php">
+                    <button onmouseover="" style="width: 100px; background-color: #029727; cursor: pointer; border-top-left-radius: 10px; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; border-top-right-radius: 10px;">
+                        Ny bruger?</button>
+                </a>
+            </div>
+            <div id="menu" style="background-color:#04B431; width:100%; float:left; text-align:center">
+                <center>
+                    <a href="sandwichshoppen.php">
+                        <button onmouseover="" style="width:400; background-color: #029727; cursor: pointer; border: 1px solid #444; border-top-left-radius: 50px;"><font size="6" color="#FFFFFF">Forside</font></button>
+                    </a>
+                    <a href="sandwichshoppen_menu.php">
+                        <button onmouseover="" style="width:400; background-color: #029727; cursor: pointer; border: 1px solid #444;"><font size="6" color="#FFFFFF">Menu</font></button>
+                    </a>
+                    <a href="sandwichshoppen_kontakt.php">
+                        <button onmouseover="" style="width:400; background-color: #029727; cursor: pointer; border: 1px solid #444; border-top-right-radius: 50px;"><font size="6" color="FFFFFF">Kontakt Information</font></button> 
+                    </a>
+                </center>
+            </div>
+            <div align="center">
+                <form>
+                    <table>
+                        <img style="height: 400"src="http://abovethelaw.com/wp-content/uploads/2013/11/uncle-sam-we-want-you.jpg">
+                        <h1>Oprettelse af ny bruger</h1>
+                        <tr><td>Brugernavn:<td><input type="text" name="username"></td></tr>
+                        <tr><td>Adgangskode:<td><input type="password" name="password"></td></tr>
+                        <tr><td>Bekr&#230ft adgangskode:<td><input type="password" name="password2"></td></tr>
+                        <tr>
+                            <td align="center" colspan="2"><input type="submit" value="Opret bruger" style="background-color: #F5F6CE; border-top-left-radius: 10px; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; border-top-right-radius: 10px;"></td></tr>
+                    </table></form>
             </div>
         </div>
-
         <div id="footer" style="background-color:#04B431; clear:both; text-align:center;">
             Valby Langgade 36 TLF: 20 78 02 52</div>
     </div>
